fa6e765c9dda9c65210230f9e4b73542e951a336
Jacob <jacobpayne007@gmail.com>
2024-08-06 10:19:19 -0500

    done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 function App() {
-  return <div>Your code here</div>;
+  return (
+    <div>
+      <h1>Hi, I'm (your name)</h1>
+      <h2>About Me</h2>
+      <p>bio</p>
+      <a href="github.com">Github</a>
+      <a href="linkedin.com">Linkedin</a>
+    </div>
+  );
 }
 
-export default App;
+export default App
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -4,3 +4,49 @@ import '@testing-library/jest-dom';
 import App from "../App";
 
 // Your tests here
+test("displays a top-level heading with the text `Hi, I'm ______`", ()=>{
+    render(<App/>)
+    const topLevelHeading = screen.getByRole("heading", {
+        name: /hi, i'm/i,
+        exact:false,
+        level:1,
+    })
+
+    expect(topLevelHeading).toBeInTheDocument();
+})
+
+test("Test for h2", ()=>{
+    render(<App/>)
+    const topLevelHeading = screen.getByRole("heading", {
+        name: /about me/i,
+        exact:false,
+        level:2,
+    })
+
+    expect(topLevelHeading).toBeInTheDocument();
+})
+test("Test for p", ()=>{
+    render(<App/>)
+    expect (screen.getByText("bio")).toBeInTheDocument
+})
+
+test("Test for linkedin Link", ()=>{
+    render(<App/>)
+    const topLevelHeading = screen.getByRole("link", {
+        name: /linkedin/i,
+        exact:false,
+        
+    })
+
+    expect(topLevelHeading).toBeInTheDocument();
+})
+test("Test for github Link", ()=>{
+    render(<App/>)
+    const topLevelHeading = screen.getByRole("link", {
+        name: /github/i,
+        exact:false,
+        
+    })
+
+    expect(topLevelHeading).toBeInTheDocument();
+})
